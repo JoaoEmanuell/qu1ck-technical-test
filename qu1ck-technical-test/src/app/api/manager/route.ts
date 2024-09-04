eb00aa09a3de2f23778d52d11175ad3e7b52c777
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2024-09-04 18:23:41 -0300

    delete: src/app/api/manager/route.ts

--- qu1ck-technical-test/src/app/api/manager/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  return Response.json({ message: "Hello World" });
-}
-
-export async function POST(request: Request) {
-  const json = await request.json();
-  return Response.json(json);
-}
